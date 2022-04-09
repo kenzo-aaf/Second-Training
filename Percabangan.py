d8b2be255d9b017ec2e998d340b8f196985284a8
aditiyahmd <aditiyahmd@gmail.com>
2022-04-09 11:03:02 +0700

    branching

---
+++ Percabangan.py
@@ -0,0 +1,18 @@
+# Training Percabangan
+
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print('Maka Budi berakngkat ke toko')
+print('Dan mulai berbelanja')
+
+jumlah_botol_susu = 10
+jumlah_telur = 1213
+if jumlah_botol_susu > 0 :
+    print('Budi membeli 1 botol susu')
+else:
+    print('Budi tidak jadi membeli 1 botol susu')
+
+print('Budi pulang ke rumah')
+print('menyerahkan hasil belanja ke pada ibu')
+
