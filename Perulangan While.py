72a37839edac5a45a01089042c082d4f2aab2652
aditiyahmd <aditiyahmd@gmail.com>
2022-04-10 09:51:13 +0700

    loop with loop and while

---
+++ Perulangan While.py
@@ -0,0 +1,11 @@
+Jumlah_buku = 10
+Jumlah_buku_yang_dibaca = 0
+
+print('Baca semua buku')
+print(f'Jumlah buku yang sudah di baca {Jumlah_buku_yang_dibaca}')
+print('Baca Buku')
+while Jumlah_buku_yang_dibaca < Jumlah_buku :
+    Jumlah_buku_yang_dibaca = Jumlah_buku_yang_dibaca +1
+    print(f'Buku ke {Jumlah_buku_yang_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah di baca {Jumlah_buku_yang_dibaca}')
---
+++ Perulangan for.py
@@ -0,0 +1,8 @@
+Jumlah_buku = 10
+Jumlah_buku_yang_dibaca = 0
+
+print('Baca semua buku')
+for Jumlah_buku_yang_dibaca in range (1, 11) :
+    print(f'Buku ke {Jumlah_buku_yang_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah di baca {Jumlah_buku_yang_dibaca}')
